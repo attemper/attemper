f9c53366323b0c023b88000fb1d584371e0a5704
ldang264 <820704815@qq.com>
2019-04-01 19:19:58 +0800

    set default value to threadPool & big change of abort spring quartz bean

--- attemper-scheduler/src/main/java/com/sse/attemper/scheduler/SchedulerApplication.java
+++ attemper-scheduler/src/main/java/com/sse/attemper/scheduler/SchedulerApplication.java
@@ -5,7 +5,6 @@ import com.sse.attemper.config.conf.ConfigConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,7 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement  //开启事务
 @EnableJpaRepositories(GlobalConstants.jpaRepositoryLocation)   //jpa dao
 @EntityScan(GlobalConstants.jpaEntityLocation)   //jpa entity
-@SpringBootApplication(exclude = {QuartzAutoConfiguration.class})
+@SpringBootApplication
 @EnableDiscoveryClient
 public class SchedulerApplication {
 
--- attemper-scheduler/src/main/java/com/sse/attemper/scheduler/conf/SchedulerConfiguration.java
+++ attemper-scheduler/src/main/java/com/sse/attemper/scheduler/conf/SchedulerConfiguration.java
@@ -2,20 +2,32 @@ package com.sse.attemper.scheduler.conf;
 
 import com.sse.attemper.autoconfigure.GrpcClientConfiguration;
 import com.sse.attemper.factory.DiscoveryClientChannelFactory;
+import com.sse.quartz.spring.boot.autoconfigure.SseQuartzAutoConfiguration;
 import io.grpc.Channel;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Import(
-        GrpcClientConfiguration.class
-)
+@Import({
+        SseQuartzAutoConfiguration.class,
+        GrpcClientConfiguration.class,
+})
 @Configuration
 public class SchedulerConfiguration {
 
     @Bean
     public Channel channel(DiscoveryClientChannelFactory channelFactory) {
         return channelFactory.create();
+    }
+
+    @Bean
+    public Scheduler scheduler(SchedulerFactory schedulerFactory) throws SchedulerException {
+        Scheduler scheduler = schedulerFactory.getScheduler();
+        scheduler.start();
+        return scheduler;
     }
 
 }
