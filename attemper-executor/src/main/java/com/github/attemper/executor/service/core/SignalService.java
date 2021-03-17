4620b8e7a3636f4abcd5d69f2916ff648987447f
ldang264 <820704815@qq.com>
2021-03-17 09:48:30 +0800

    remove unused inject

--- attemper-executor/src/main/java/com/github/attemper/executor/service/core/SignalService.java
+++ attemper-executor/src/main/java/com/github/attemper/executor/service/core/SignalService.java
@@ -1,18 +1,13 @@
 package com.github.attemper.executor.service.core;
 
-import com.github.attemper.core.service.instance.InstanceService;
 import com.github.attemper.executor.store.ExecutorStore;
 import com.github.attemper.java.sdk.common.executor.param.execution.EndParam;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class SignalService {
-
-    @Autowired
-    private InstanceService service;
 
     public Void signal(EndParam endParam) {
         ExecutorStore.getEndMap().put(endParam.getBaseExecutionParam().getExecutionId(), endParam);
